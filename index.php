e069324e0557d2dd74482da5d0db45ed979c2208
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 15:01:07 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+          function nameAge($name, $age) {
+            echo $name . ' a ' . $age . ' ans';
+          }
+
+          nameAge('Mohamed', 30);
+       ?>
+    </p>
+  </body>
+</html>
